4675f61e34d0025d6dee2f2600ee33dfd74a0ef0
kiran364 <kumawatkiran364@gmail.com>
2021-08-27 10:34:18 +0530

    javascript higher order function exampal with function reusability

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
-//array rotation
-function rotation(arr) {
-    for(let i=0; i<4; i++) {
-        var el = arr.shift();
-        arr.push(el);    
-        console.log(arr);  
-    }
+// High order function example
+var redius = [3,4,8,6];
+
+const area = (redius) => {
+    return Math.PI * redius * redius;
 }
-var arr = [1,2,3,4];
-rotation(arr);
+
+const circum = (redius) => {
+    return 2 * Math.PI * redius;
+}
+
+const diameter = (redius) => {
+    return 2 * redius;
+}
+
+const calculate = (redius, logic) => {
+    const output = [];
+    for(let i=0; i<redius.length; i++) {
+        output.push(logic(redius[i]));
+    }
+    return output;
+}
+
+console.log(calculate(redius, area));
+console.log(calculate(redius, diameter));
+console.log(calculate(redius, circum));
+
+
