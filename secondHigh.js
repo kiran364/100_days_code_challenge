cf874ad4980dc1420ff8f02fbfb283563b3d3478
kiran364 <kumawatkiran364@gmail.com>
2021-09-01 13:46:04 +0530

    javascript function to print second highest number from given array

--- secondHigh.js
+++ secondHigh.js
@@ -1,14 +1,18 @@
 function secondHigh(arr) {
-    let max = [];
+    let temp;
     for(let i = 0; i <arr.length; i++) {
-        for(let j=1; j<arr.length; j++) {
-                if(arr[i] < arr[j]) {
-                max.push(arr[j]);
+        for(let j=i+1; j<arr.length; j++) {
+            if(arr[i] > arr[j]) {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
         }
     }
-    return max[max.length-2];
-  }
+    return arr;
+}
 
-const arr = [1,2,3,4,5,6,7,2];
-console.log(secondHigh(arr));
+const arr = [11,2,3,4,5,6,7];
+var result = secondHigh(arr);
+console.log(result)
+console.log(result[result.length - 2]);
