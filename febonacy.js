e623e71d3a6c9f9963f624570335d0e54c93e506
kiran364 <kumawatkiran364@gmail.com>
2021-09-02 11:47:49 +0530

    javascript function to print the fibonacci sequence

---
+++ febonacy.js
@@ -0,0 +1,15 @@
+// program to generate fibonacci series up to n terms
+// The Fibonacci sequence is the integer sequence where
+//  the first two terms are 0 and 1. After that, the next term is defined 
+//  as the sum of the previous two terms.
+const number = 8;
+let n1 = 0, n2 = 1, nextTerm;
+
+console.log('Fibonacci Series:');
+
+for (let i = 1; i <= number; i++) {
+    console.log(n1);
+    nextTerm = n1 + n2;
+    n1 = n2;
+    n2 = nextTerm;
+}
