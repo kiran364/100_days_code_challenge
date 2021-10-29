e707aaa22f0bb55c36a190150577fa8bd34e4024
kiran364 <79898690+kiran364@users.noreply.github.com>
2021-10-29 11:57:15 +0530

    updated

--- febonacy.js
+++ febonacy.js
@@ -8,7 +8,7 @@ let n1 = 0, n2 = 1, nextTerm;
 console.log('Fibonacci Series:');
 
 for (let i = 1; i <= number; i++) {
-    console.log(n1);
+    console.log(n2);
     nextTerm = n1 + n2;
     n1 = n2;
     n2 = nextTerm;
