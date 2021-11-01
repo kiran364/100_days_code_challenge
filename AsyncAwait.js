6d0f5bb44850698f8ac5e834ffca7f03de4377c3
kiran364 <kumawatkiran364@gmail.com>
2021-11-01 19:58:54 +0530

    asinc await updated

--- AsyncAwait.js
+++ AsyncAwait.js
@@ -1,19 +1,19 @@
-console.log('start')
-
-function a(x){
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(console.log(x))
-    },3000)
+const abc = (num) => {
+  return promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+          resolve(num);
+      }, 2000);
   })
 }
-    
 
-async function b(){
- await  a('a');
- await  a('b');
- await  a('c');
+const xyz = async () => {
+  
+  await abc(1).then((num) => console.log(num));
+
+  await abc(2).then((num) => console.log(num));
+  await abc(3).then((num) => console.log(num));
+  await abc(4).then((num) => console.log(num));
+
 }
-
-b();
+xyz();
 
