b05dad5f84370970ea9fade328a5a2ca35cea297
kiran364 <kumawatkiran364@gmail.com>
2021-11-25 21:39:26 +0530

    javascript program to print second highest number from array

--- secondHigh2.js
+++ secondHigh2.js
@@ -1,16 +1,18 @@
-const arr = [101, 101, 1, 24, 5, 15 ,25 , 45, 66, 88, 98, 101, 98, 1];
+// const arr = [101, 101, 1, 24, 5, 15 ,25 , 45, 66, 88, 98, 101, 98, 1];
 
-const secondLarge = (array) => {
-    let firstLarge = 0;
-    let secondLarge = 0;
-    for(let i=0; i<array.length; i++){
-        if(firstLarge < array[i]){
-            secondLarge = firstLarge;
-            firstLarge = array[i];
-        }else if(array[i] > secondLarge && array[i] != firstLarge){
-            secondLarge = array[i];
-        }
-    }
-    return secondLarge;
-}
-console.log(secondLarge(arr));
+// const secondLarge = (array) => {
+//     let firstLarge = 0;
+//     let secondLarge = 0;
+//     for(let i=0; i<array.length; i++){
+//         if(firstLarge < array[i]){
+//             secondLarge = firstLarge;
+//             firstLarge = array[i];
+//         }else if(array[i] > secondLarge && array[i] != firstLarge){
+//             secondLarge = array[i];
+//         }
+//     }
+//     return secondLarge;
+// }
+// console.log(secondLarge(arr));
+
+
